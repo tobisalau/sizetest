f7cfa1a5fa8878f14fb15987f18e55b36d4cb018
Tobi Salau <112990447+tobisalau@users.noreply.github.com>
2023-02-11 22:53:10 +0000

    Add files via upload

---
+++ script1.php
@@ -0,0 +1,70 @@
+<?php
+
+function errHan($error) {
+    if (empty($firstErr)) {
+        $firstErr = $error;
+    }
+}
+
+function vali_input($info) {
+    $info = trim($info);
+    $info = stripslashes($info);
+    $info = htmlspecialchars($info);
+    return $info;
+}
+
+$uname = $pword = "";
+$firstErr = "";
+
+if (isset($_POST["SUBMIT"])) {
+    $uname = $pword = "";
+    $firstErr = "";
+    
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        errHan("Please enter a username");
+    }
+    else {
+        $uname = vali_input($_POST["username"]);
+        if (!preg_match("/^[a-zA-Z0-9_]*$/",$uname)) {
+            errHan("Only letters, number and underscores are allowed in usernames");
+        }
+    }
+    
+    if (empty($_POST["pwrd"])) {
+        errHan("Please enter a password");
+    }
+    else {
+        $pword = vali_input($_POST["pwrd"]);;
+    }
+    if (!empty($firstErr)) {
+        header("location: index.php?error=" . $firstErr);
+        exit();
+   }
+    // $connection = obdc_connect('DRIVER={ODBC Driver 18 for SQL Server};Server=tcp:sizeserver2.database.windows.net,1433;DATABASE=sizedb5;UID=ooas3;PWD=Password22!!;CONNECTION TIMEOUT=30;');
+}
+}
+
+
+
+/* 
+    $serverName = "sizeserver2.database.windows.net"; 
+    $connectionOptions = array(
+        "Database" => "sizedb5", 
+        "Uid" => "ooas3", 
+        "PWD" => "Password22!!" 
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT brandname FROM Brands WHERE b_id == 1";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo $getResults
+    /* if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($ getResults); */
+
+?>
+    
