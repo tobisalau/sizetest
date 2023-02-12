9904c21623ec2eee89171c04d26a31e0038a2c95
Tobi Salau <112990447+tobisalau@users.noreply.github.com>
2023-02-12 00:28:28 +0000

    Update script1.php

--- script1.php
+++ script1.php
@@ -16,37 +16,33 @@ function vali_input($info) {
 }
 
 if (isset($_POST["submit"])) {
-    $uname = $pword = "0";
-    $firstErr = "0";
+    $uname = $pword = "";
+    $firstErr = "";
     
     if (empty($_POST["name"])) {
         errHan("Please enter a username");
-        header("location: index.php?error=emptyname");
+        header("location: https://sizetest.azurewebsites.net/index.php?error=emptyname");
         exit();
     }
     else {
         $uname = vali_input($_POST["username"]);
         if (!preg_match("/^[a-zA-Z0-9_]*$/",$uname)) {
             errHan("Only letters, number and underscores are allowed in usernames");
+            header("location: https://sizetest.azurewebsites.net/index.php?error=Invalid-Username");
+        exit();
         }
     }
     
     if (empty($_POST["pwrd"])) {
         errHan("Please enter a password");
+        header("location: https://sizetest.azurewebsites.net/index.php?error=emptypassword");
+        exit();
     }
     else {
         $pword = vali_input($_POST["pwrd"]);;
     }
-    if (!(empty($firstErr))) {
-        $pth = "location:failsign.php?error=" . $firstErr;
-        header($pth);
-        exit();
-       else {
-           $pth = "location:failsign.php?error=letshearit";
-        header($pth);
-        exit();
-       }
-   }
+   
+   
     // $connection = obdc_connect('DRIVER={ODBC Driver 18 for SQL Server};Server=tcp:sizeserver2.database.windows.net,1433;DATABASE=sizedb5;UID=ooas3;PWD=Password22!!;CONNECTION TIMEOUT=30;');
 }
 
