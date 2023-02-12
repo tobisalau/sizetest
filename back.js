fe1cc801478f0aa35764a800786a18446e51ced4
Tobi Salau <112990447+tobisalau@users.noreply.github.com>
2023-02-12 15:07:42 +0000

    Update back.js

--- back.js
+++ back.js
@@ -7,3 +7,14 @@ function getStarted() {
     document.getElementById("page2intro").classList.add('sel1');
 }
 
+function toLog() {
+    document.getElementById("singy").style.display="none";
+    document.getElementById("loggy").style.display="flex";
+    
+}
+function toSign() {
+    document.getElementById("loggy").style.display="none";
+    document.getElementById("singy").style.display="flex";
+
+}
+
